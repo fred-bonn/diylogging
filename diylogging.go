b2baa05140d1eeacbd538d13b42fb592a3bdaef3
fred-bonn <sadpantz@gmail.com>
2026-03-23 22:44:45 +0100

    removed the sync import need from packages using this library

--- diylogging.go
+++ diylogging.go
@@ -3,11 +3,27 @@ package diylogging
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
-func LogStart(ch chan string) {
+type LogString string
+
+func (ls LogString) String() string {
+	return string(ls)
+}
+
+func LogStart() (chan<- fmt.Stringer, *sync.WaitGroup) {
+	ch := make(chan fmt.Stringer)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go log(ch, &wg)
+	return ch, &wg
+}
+
+func log(ch chan fmt.Stringer, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for msg := range ch {
-		fmt.Println("%s: %s", time.Now().Format(time.RFC3339), msg)
+		fmt.Printf("%s | %s\n", time.Now().Format(time.RFC3339), msg.String())
 	}
 	fmt.Println("Logging stopped.")
 }
