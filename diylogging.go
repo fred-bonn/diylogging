06957005e769045e688a6f266ade03e0bd67c89d
fred-bonn <sadpantz@gmail.com>
2026-03-25 16:54:01 +0100

    added a timer function

--- diylogging.go
+++ diylogging.go
@@ -2,28 +2,61 @@ package diylogging
 
 import (
 	"fmt"
-	"time"
 	"sync"
+	"time"
 )
 
-type LogString string
-
-func (ls LogString) String() string {
-	return string(ls)
-}
-
-func LogStart() (chan<- fmt.Stringer, *sync.WaitGroup) {
-	ch := make(chan fmt.Stringer)
+func LogStart() (chan<- any, *sync.WaitGroup) {
+	ch := make(chan any, 100)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go log(ch, &wg)
 	return ch, &wg
 }
 
-func log(ch <-chan fmt.Stringer, wg *sync.WaitGroup) {
+func log(ch <-chan any, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for msg := range ch {
-		fmt.Printf("%s | %s\n", time.Now().Format(time.RFC3339), msg.String())
+	for message := range ch {
+		switch value := message.(type) {
+		case fmt.Stringer:
+			fmt.Printf("%s | %s\n", time.Now().Format(time.RFC3339), value.String())
+		case string:
+			fmt.Printf("%s | %s\n", time.Now().Format(time.RFC3339), value)
+		default:
+			fmt.Printf("%s | %v\n", time.Now().Format(time.RFC3339), value)
+
+		}
 	}
-	fmt.Println("Logging stopped.")
+}
+
+func TimerStart() (chan<- any, *sync.WaitGroup) {
+	ch := make(chan any, 100)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go timer(ch, &wg)
+	return ch, &wg
+}
+
+func timer(ch <-chan any, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var ongoing bool
+	var startTime time.Time
+	var tempDuration time.Duration
+	var totalDuration time.Duration
+	globalTime := time.Now()
+
+	for range ch {
+		if !ongoing {
+			ongoing = true
+			startTime = time.Now()
+		} else {
+			tempDuration = time.Since(startTime)
+			totalDuration += tempDuration
+			fmt.Printf("Interval: %vns\n", tempDuration.Nanoseconds())
+			ongoing = false
+		}
+	}
+
+	fmt.Printf("Total Time: %v\n", totalDuration)
+	fmt.Printf("Ratio: %.3f%%\n", float64(totalDuration.Nanoseconds())/float64(time.Since(globalTime).Nanoseconds())*100)
 }
